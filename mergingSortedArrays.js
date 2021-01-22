288f98b7dee9f4d884b6679167e75973985c811b
shraddharocks6 <shraddha6pandey@gmail.com>
2021-01-22 19:29:33 +0530

    D

--- mergingSortedArrays.js
+++ mergingSortedArrays.js
@@ -8,29 +8,23 @@
 
 function merge(arr1, arr2) {
   var mergedArray = [];
-  var i = 0,
-    j = 0;
+  var i = 0;
+  var j = 0;
+
   while (i < arr1.length && j < arr2.length) {
-    if (arr1[i] <= arr2[j]) {
-      mergedArray.push(arr1[i++]);
-    } else {
-      mergedArray.push(arr2[j++]);
-    }
+    if (arr1[i] <= arr2[j]) mergedArray.push(arr1[i++]);
+    else mergedArray.push(arr2[j++]);
   }
 
-  if (i < arr1.length) {
-    mergedArray = mergedArray.concat(arr1.slice(i));
-  }
-  if (j < arr2.length) {
-    mergedArray = mergedArray.concat(arr2.slice(j));
-  }
+  if (i < arr1.length) mergedArray = mergedArray.concat(arr1.slice(i));
+  if (j < arr2.length) mergedArray = mergedArray.concat(arr2.slice(j));
+
+  return mergedArray;
 
   //   if (arr1.length !== arr2.length)
   //     return arr1.length > arr2.length
   //       ? mergedArray.concat(arr1.slice(arr2.length - 1))
   //       : mergedArray.concat(arr2.slice(arr1.length - 1));
-
-  return mergedArray;
 }
 
 arr1 = [1, 3, 5, 7, 9, 10, 11, 15, 19, 25];
